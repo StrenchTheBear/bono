e07e98278ce78f34458afd9b7ae62356b03d0550
StrenchTheBear <acliv2904@yahoo.com>
2020-10-18 17:56:27 -0500

    fix

--- Models/bono.cs
+++ Models/bono.cs
@@ -8,27 +8,39 @@ namespace BONO.Models
     public class bono
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("id")]
+        
         [Required(ErrorMessage = "Por favor, ingrese el Nombre")]
         [Display(Name="Nombre")]
+        [Column("Nombre")]
         public String Name { get; set; }
 
         [Required(ErrorMessage = "Por favor, ingrese el Apellido")]
         [Display(Name="Apellido")]
+        [Column("Apellido")]
         public String LastName { get; set; }
 
         [Required(ErrorMessage = "Por favor, ingrese el Distrito")]
         [Display(Name="Distrito")]
+        [Column("Distrito")]
         public String Distrito { get; set; }
         
+        
+        [Required(ErrorMessage = "Por favor, ingrese su banco")]
+        [Display(Name="banco")]
         public String banco { get; set; }
+        [Column("banco")]
+
 
         [Required(ErrorMessage = "Por favor, ingrese su Edad")]
-        [Display(Name="Edad")]
+        [Display(Name="Edad")]             
+        
         public int Edad { get; set; }
 
+       [Required(ErrorMessage = "Por favor, ingrese su genero")]
+        [Display(Name="genero")]  
         public String genero { get; set; }
 
+       
         public String Response { get; set; }
 
     }
